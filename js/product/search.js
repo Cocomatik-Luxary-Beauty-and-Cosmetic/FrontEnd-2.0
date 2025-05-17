2895243abc44adc5528e571ee726beee17fbee68
schdule-x <adarshananda51@gmail.com>
2025-05-17 17:21:36 +0530

    //

--- js/product/search.js
+++ js/product/search.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         // Render filtered products
         filteredProducts.forEach(product => {
             const productCard = document.createElement("div");
-            productCard.id = "products-card";
+            productCard.id = "search-products-card";
             productCard.dataset.sku = product.sku;
 
             productCard.innerHTML = `
